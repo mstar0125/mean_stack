d531e37faaf70406bb1fb23c2ad3c4377eee1019
Zhong <mac@macs-Mac-mini.local>
2017-03-17 23:14:02 +0800

    Update cert and key name

--- app/pushnotification.js
+++ app/pushnotification.js
@@ -7,8 +7,8 @@ var DeviceToken = require('./models/deviceToken.js')
 // var	pushkey = 'app/NewPuntrPushKey.pem';
 
 var pushserverurl = 'gateway.sandbox.push.apple.com';
-var pushcert = 'app/NewDevPuntrPushChatCert.pem';
-var pushkey = 'app/NewDevPuntrPushChatKey.pem';
+var pushcert = 'app/NewDevPuntrPushCert.pem';
+var pushkey = 'app/NewDevPuntrPushKey.pem';
 
 var PushServer = new function(){
 	this.apn = require('apn');
